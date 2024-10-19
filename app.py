caa9e7c35bb10435610ff68247a89599c75c07dd
Naveen Yarlagadda <nyar99@gmail.com>
2024-10-18 22:39:31 -0400

    1

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from most_played_note import most_played_note
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/most_played_note')
+def hello_world():
+    return f'Most played note: {most_played_note()}'
